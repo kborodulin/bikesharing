d912cc258e27b0e1ed61ead899e4444d8eea0158
k.borodulin.stc <k.borodulin.stc@innopolis.university>
2019-09-14 21:45:00 +0500

    TestController

---
+++ src/main/java/ru/stc15/config/ConfigWebApp.java
@@ -0,0 +1,9 @@
+package ru.stc15.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.stc15")
+public class ConfigWebApp {
+}
